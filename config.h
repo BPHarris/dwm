5e7f9cd3e2ce8b1d005d360640a9c94fae09bcfb
bpharris <bpharris@pm.me>
2023-02-14 23:05:45 +0000

    apply: dwm-scratchpads-20200414-728d397b.diff

--- config.h
+++ config.h
@@ -23,6 +23,16 @@ static const char *colors[][4]      = {
 	[SchemeUrg]  = { col_gray4,    col_cyan,     col_urgborder, col_urgborder  },
 };
 
+const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "ranger", NULL };
+const char *spcmd3[] = {"keepassxc", NULL };
+static Scratchpad scratchpads[] = {
+	/* name          cmd  */
+	{"spterm",      spcmd1},
+	{"spranger",    spcmd2},
+	{"keepassxc",   spcmd3},
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
@@ -31,11 +41,18 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
+	/* Note
+	 * St with Xresources patch is 'St' otherwise it is 'st'
+	 */
 	/* class      instance    title       tags mask     isfloating   isfreesize   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,	      -1 },
-	{ "st",       NULL,       NULL,       0,    	    0,           0,	      -1 },
-	{ "St",       NULL,       NULL,       0,	    0,           0,	      -1 }, /* St with Xresources patch */
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	{ "st",       NULL,       NULL,       0,    	    0,           0,           -1 },
+	{ "St",       NULL,       NULL,       0,            0,           0,           -1 },
+	/* scratchpads */
+	{ NULL,       "spterm",   NULL,       SPTAG(0),     1,           1,           -1 },
+	{ NULL,       "spfm",     NULL,       SPTAG(1),     1,           1,           -1 },
+	{ NULL,       "keepassxc",NULL,       SPTAG(2),     0,           1,           -1 },
 };
 
 /* layout(s) */
@@ -107,6 +124,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
+	{ MODKEY,                       XK_y,      togglescratch,  {.ui = 0 } },
+	{ MODKEY,                       XK_u,      togglescratch,  {.ui = 1 } },
+	{ MODKEY,                       XK_x,      togglescratch,  {.ui = 2 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -129,7 +149,7 @@ static const Button buttons[] = {
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
