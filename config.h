eadd21204d19dd96939534badcd396bbe2e94aa5
bpharris <bpharris@pm.me>
2023-03-14 22:05:13 +0000

    add: use rofi over dmenu

--- config.h
+++ config.h
@@ -122,14 +122,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+
+static const char *druncmd[] = { "rofi", "-show", "drun", NULL };
+static const char *runcmd[] = { "rofi", "-show", "run", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", c_dark_grey, "-nf", c_white, "-sb", c_flat_green, "-sf", c_off_white, NULL };
-static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_r,      spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = druncmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = runcmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
